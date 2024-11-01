01834c03a649be962183d1fb9028769bd81fa1c6
Landice Fu <landice.fu@cardinalblue.com>
2024-11-01 17:06:49 +0800

    Create :lib module and let :demo depend on it

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -50,20 +50,30 @@ android {
 }
 
 dependencies {
+    implementation(project(":lib"))
 
+    // Jetpack
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+
+    // Compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.material3)
+
+    // Adding GPUImage as a reference for development.
+    implementation(libs.image.processing.gpuImage)
+
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "KraftShader"
 include(":demo")
- 
+include(":lib")
