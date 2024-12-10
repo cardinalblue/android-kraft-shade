c854ad173ec47da60a98f154ebd4ffe5e225dff3
Landice Fu <landice.fu@cardinalblue.com>
2024-12-10 12:07:23 +0800

    feat: Add a single value constructor to GlSizeF

--- kraft-shade/src/main/java/com/cardinalblue/kraftshade/model/GlSizeF.kt
+++ kraft-shade/src/main/java/com/cardinalblue/kraftshade/model/GlSizeF.kt
@@ -4,6 +4,8 @@ data class GlSizeF(
     val width: Float,
     val height: Float,
 ) {
+    constructor(size: Float) : this(size, size)
+
     val aspectRatio: Float get() = width / height
     val area: Float get() = width * height
 
