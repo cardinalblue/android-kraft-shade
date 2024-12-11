5770d8232f403f8ff1659a0d0e7c70fad92c4a80
Landice Fu <landice.fu@cardinalblue.com>
2024-12-11 14:56:13 +0800

    feat: Add DoNothingKraftShader for scaling

--- kraft-shade/src/main/java/com/cardinalblue/kraftshade/model/OptionalBitmap.kt
+++
@@ -1,5 +0,0 @@
-package com.cardinalblue.kraftshade.model
-
-import android.graphics.Bitmap
-
-class OptionalBitmap(val bitmap: Bitmap?)
---
+++ kraft-shade/src/main/java/com/cardinalblue/kraftshade/shader/builtin/DoNothingKraftShader.kt
@@ -0,0 +1,23 @@
+package com.cardinalblue.kraftshade.shader.builtin
+
+import com.cardinalblue.kraftshade.shader.TextureInputKraftShader
+import org.intellij.lang.annotations.Language
+
+/**
+ * This can be used to resize from buffer to another buffer
+ */
+class DoNothingKraftShader : TextureInputKraftShader() {
+    override fun loadFragmentShader(): String = DO_NOTHING_FRAGMENT_SHADER
+}
+
+@Language("GLSL")
+private const val DO_NOTHING_FRAGMENT_SHADER = """
+varying highp vec2 textureCoordinate;
+
+uniform sampler2D inputImageTexture;
+
+void main()
+{
+     gl_FragColor = texture2D(inputImageTexture, textureCoordinate);
+}
+"""
