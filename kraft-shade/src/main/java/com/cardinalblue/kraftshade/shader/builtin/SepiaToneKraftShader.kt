d1f92953f6ce41955586a81be3c56dcd73af484a
Yuji Wong <yuji.wong@cardinalblue.com>
2024-12-19 17:12:42 +0800

    feat: Add sepia tone shader

--- kraft-shade/src/main/java/com/cardinalblue/kraftshade/shader/builtin/SepiaToneKraftShader.kt
+++ kraft-shade/src/main/java/com/cardinalblue/kraftshade/shader/builtin/SepiaToneKraftShader.kt
@@ -1,4 +1,13 @@
 package com.cardinalblue.kraftshade.shader.builtin
 
-class SepiaToneKraftShader {
-}
+import com.cardinalblue.kraftshade.model.GlMat4
+
+class SepiaToneKraftShader(intensity: Float = 1.0f) : ColorMatrixKraftShader(
+    intensity = intensity,
+    colorMatrix = GlMat4(
+        0.3588f, 0.7044f, 0.1368f, 0.0f,
+        0.2990f, 0.5870f, 0.1140f, 0.0f,
+        0.2392f, 0.4696f, 0.0912f, 0.0f,
+        0f, 0f, 0f, 1.0f
+    )
+)
